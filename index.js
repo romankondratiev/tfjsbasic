7d56a6fe7abee53f06a925421976c66667ae2f70
Roman Kondratiev <romhedoo@gmail.com>
2018-10-03 00:48:37 +0200

    fdfdsf

--- index.js
+++ index.js
@@ -4,10 +4,7 @@
   mobilenet.load().then(model => {
     // Classify the image.
     model.classify(img).then(predictions => {
-      console.log('Predictions: ');
-      console.log(predictions);
-    document.getElementById('micro_out_div').innerText +=
-    model.classify(img);
+      document.getElementById('micro_out_div').innerText += predictions;
     });
   });
 
