c350f975a2e5902e3585014806dbb89897428f03
romankondratiev <32718126+romankondratiev@users.noreply.github.com>
2018-10-04 15:17:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,7 @@
     model.classify(img).then(predictions => {
       console.log('Predictions: ');
       console.log(predictions);
-      console.log('obj', predictions);
-      console.log('.toString', predictions.toString());
-      console.log('String()', String(predictions));
-      console.log('JSON.stringify',JSON.stringify(predictions));
+      console.log(JSON.stringify(predictions));
     });
   });
 
