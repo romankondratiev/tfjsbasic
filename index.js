b6338bb9df92d2f01b38ff3eab1b283c6cbd4cfb
romankondratiev <32718126+romankondratiev@users.noreply.github.com>
2018-10-04 11:06:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 
   const img = document.getElementById('img');
+  const img = document.getElementById('img');
   // Load the model.
   mobilenet.load().then(model => {
     // Classify the image.
     model.classify(img).then(predictions => {
-      document.write(predictions.name);
+      console.log('Predictions: ');
+      console.log(predictions);
+      console.log('obj', predictions);
+      console.log('.toString', predictions.toString());
+      console.log('String()', String(predictions));
+      console.log('JSON.stringify',JSON.stringify(predictions));
     });
   });
 
